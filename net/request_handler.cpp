06daba1e21d334a434638676592424d34eb4f3c7
Windoze <windoze.net@gmail.com>
2012-12-24 20:39:43 +0800

    Remove unused code

--- net/request_handler.cpp
+++ net/request_handler.cpp
@@ -33,65 +33,10 @@ namespace http {
         
         void request_handler::handle_request(const request& req, reply& rep)
         {
-#if 1
-            url_handler *h=get_url_handler(req.uri);
+            url_handler_ptr h=get_url_handler(req.uri);
             if (h) {
                 h->handle_request(req, rep);
             }
-#else
-            // Decode url to path.
-            std::string request_path;
-            if (!url_decode(req.uri, request_path))
-            {
-                rep = reply::stock_reply(reply::bad_request);
-                return;
-            }
-            
-            // Request path must be absolute and not contain "..".
-            if (request_path.empty() || request_path[0] != '/'
-                || request_path.find("..") != std::string::npos)
-            {
-                rep = reply::stock_reply(reply::bad_request);
-                return;
-            }
-            
-            // If path ends in slash (i.e. is a directory) then add "index.html".
-            if (request_path[request_path.size() - 1] == '/')
-            {
-                request_path += "index.html";
-            }
-            
-            // Determine the file extension.
-            std::size_t last_slash_pos = request_path.find_last_of("/");
-            std::size_t last_dot_pos = request_path.find_last_of(".");
-            std::string extension;
-            if (last_dot_pos != std::string::npos && last_dot_pos > last_slash_pos)
-            {
-                extension = request_path.substr(last_dot_pos + 1);
-            }
-            
-            // Open the file to send back.
-            std::string full_path = doc_root_ + request_path;
-            std::ifstream is(full_path.c_str(), std::ios::in | std::ios::binary);
-            if (!is)
-            {
-                rep = reply::stock_reply(reply::not_found);
-                return;
-            }
-            
-            // Fill out the reply to be sent to the client.
-            rep.status = reply::ok;
-            char buf[512];
-            while (is.read(buf, sizeof(buf)).gcount() > 0)
-            {
-                rep.content.append(buf, is.gcount());
-            }
-            rep.headers.resize(2);
-            rep.headers[0].name = "Content-Length";
-            rep.headers[0].value = boost::lexical_cast<std::string>(rep.content.size());
-            rep.headers[1].name = "Content-Type";
-            rep.headers[1].value = mime_types::extension_to_type(extension);
-#endif
         }
         
         bool request_handler::url_decode(const std::string& in, std::string& out)
